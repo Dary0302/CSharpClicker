216a3825c5dc4a0f46fbe953c6d1e493f9ca564d
Dary <Leontyev0302@mail.ru>
2024-09-25 20:35:35 +0500

    init CSharpClicker
    Project for course from "Очень Интересно"

---
+++ CSharpClickerWeb/Program.cs
@@ -0,0 +1,17 @@
+namespace CSharpClickerWeb;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddHealthChecks();
+        
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+        app.MapHealthChecks("health-check");
+        
+        app.Run();
+    }
+}
