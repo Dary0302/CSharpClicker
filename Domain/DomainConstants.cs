40de01f1a66473d90a9bfaf565acb34537b80434
Dary <Leontyev0302@mail.ru>
2024-12-20 15:14:37 +0500

    update structure solution

--- Domain/DomainConstants.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CSharpClicker.Web.Domain;
-
-public class DomainConstants
-{
-    public const float BoostCostModifier = 1.15f;
-}
--- UseCases/AddPoints/AddPointsCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using CSharpClicker.Web.UseCases.Common;
-using MediatR;
-
-namespace CSharpClicker.Web.UseCases.AddPoints;
-
-public record AddPointsCommand(int Clicks, int Seconds) : IRequest<ScoreDto>;
